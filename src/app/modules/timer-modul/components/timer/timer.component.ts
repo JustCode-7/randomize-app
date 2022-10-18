ca0c203073a9f6c8e132d9bda57cffd5514952f1
JustCode-7 <72110343+JustCode-7@users.noreply.github.com>
2022-10-18 20:48:42 +0200

    Timer optimized

--- src/app/modules/timer-modul/components/timer/timer.component.ts
+++ src/app/modules/timer-modul/components/timer/timer.component.ts
@@ -10,44 +10,48 @@ import {ProgressSpinnerMode} from '@angular/material/progress-spinner';
   styleUrls: ['./timer.component.scss']
 })
 export class TimerComponent {
-  timeForm: FormGroup; // Declare the signupForm
+  timeForm: FormGroup;
   forms: string[]
-  feierabend: string = "feierabend";
+  feierabendDateString: string = "feierabend";
   pausenzeit: string = "pausenzeit";
   extraPause = false;
-  color: ThemePalette = 'primary';
-  mode: ProgressSpinnerMode = 'determinate';
-  value = 0;
+  progressSpinnerColor: ThemePalette = 'primary';
+  progressSpinnerMode: ProgressSpinnerMode = 'determinate';
+  progressSpinnerValue = 0;
   maxSpinnerValue = 0;
-  timetoWork = "";
+  timetoWorkDateString = "";
   timerId = 1;
+  startTimeArr: string[] = [];
+  workTimeArr: string[] = [];
+  pauseArr: string[] = [];
+  vergangen = 0;
+  remaining = '';
 
   constructor(private fb: FormBuilder) {
     this.forms = [
       "starttime", "worktime", "pause"
     ]
     this.timeForm = this.fb.group({
-      starttime: [this.getFormattedTimestring(9, 30), [Validators.required]],
-      worktime: [this.getFormattedTimestring(7, 48), [Validators.required]],
-      pause: [this.getFormattedTimestring(0, 30), [Validators.required]]
+      starttime: [this.getFormattedTimeString(9, 30), [Validators.required]],
+      worktime: [this.getFormattedTimeString(7, 48), [Validators.required]],
+      pause: [this.getFormattedTimeString(0, 30), [Validators.required]]
     });
   }
 
-  public onFormSubmit() {
+  public calculateTotal() {
     if (this.timeForm.valid) {
-      let startTimeArr: string[] = this.timeForm.get("starttime")?.value.split(":");
-      let workTimeArr: string[] = this.timeForm.get("worktime")?.value.split(":");
-      let pauseArr: string[] = this.timeForm.get("pause")?.value.split(":");
-      let hours = this.getParseInt(startTimeArr[0]) + this.getParseInt(workTimeArr[0]) + this.getParseInt(pauseArr[0]);
-      let minutes = this.getParseInt(startTimeArr[1]) + this.getParseInt(workTimeArr[1]) + this.getParseInt(pauseArr[1]);
-      let work = this.getFormattedTimestring(this.getParseInt(workTimeArr[0]), this.getParseInt(workTimeArr[1]));
-      this.timetoWork = work;
-      this.feierabend = this.getFormattedTimestring(hours, minutes);
-      this.checkForExtraPause(work, pauseArr);
-      this.pausenzeit = this.getFormattedTimestring(this.getParseInt(pauseArr[0]), this.getParseInt(pauseArr[1]));
-      this.getSpinnerMaxValue(workTimeArr, pauseArr);
-      this.setSpinnerValue(startTimeArr);
-      this.timeloop(startTimeArr)
+      this.startTimeArr = this.timeForm.get("starttime")?.value.split(":");
+      this.workTimeArr = this.timeForm.get("worktime")?.value.split(":");
+      this.pauseArr = this.timeForm.get("pause")?.value.split(":");
+      let hours = this.getSumOfHours();
+      let minutes = this.getSumOfMinutes();
+      this.timetoWorkDateString = this.getFormattedTimeString(this.getParseInt(this.workTimeArr[0]), this.getParseInt(this.workTimeArr[1]));
+      this.feierabendDateString = this.getFormattedTimeString(hours, minutes);
+      this.feierabendDateString = this.getFormattedTimeString(hours, minutes);
+      this.calculatePause();
+      this.getSpinnerMaxValue();
+      this.setSpinnerValue(this.startTimeArr);
+      this.timeLoop(this.startTimeArr);
     }
   }
 
@@ -55,43 +59,65 @@ export class TimerComponent {
     let currentZeit = new Date().getTime();
     let startZeit = new Date().setHours(this.getParseInt(startTimeArr[0]), this.getParseInt(startTimeArr[1]));
     let vergangenInMinuten = (currentZeit - startZeit) / 60000;
+    this.vergangen = vergangenInMinuten;
     return vergangenInMinuten * 100 / this.maxSpinnerValue;
   }
 
-  private checkForExtraPause(work: string, pauseArr: string[]) {
-    if (work > this.getFormattedTimestring(9, 0)) {
-      this.extraPause = true;
-      pauseArr[1] = (this.getParseInt(pauseArr[1]) + 15).toString();
-    } else {
-      this.extraPause = false;
+  private getSumOfHours() {
+    return this.getParseInt(this.startTimeArr[0]) + this.getParseInt(this.workTimeArr[0]) + this.getParseInt(this.pauseArr[0]);
+  }
+
+  private getSumOfMinutes() {
+    return this.getParseInt(this.startTimeArr[1]) + this.getParseInt(this.workTimeArr[1]) + this.getParseInt(this.pauseArr[1]);
+  }
+
+  private calculatePause() {
+    this.extraPause = false;
+    if ((this.timetoWorkDateString > this.getFormattedTimeString(6, 0)) && this.getParseInt(this.pauseArr[1]) < 30) {
+      this.pauseArr[1] = "30";
     }
+    if ((this.timetoWorkDateString > this.getFormattedTimeString(9, 0)) && this.getParseInt(this.pauseArr[1]) < 45) {
+      this.extraPause = true;
+      this.pauseArr[1] = (this.getParseInt(this.pauseArr[1]) + 15).toString();
+    }
+    this.pausenzeit = this.getFormattedTimeString(this.getParseInt(this.pauseArr[0]), this.getParseInt(this.pauseArr[1]));
+    this.timeForm.controls['pause'].setValue(this.pausenzeit);
+
   }
 
   private setSpinnerValue(startTimeArr: string[]) {
-    if (this.value <= 100) {
-      this.value = this.getSpinnerValue(startTimeArr);
+    if (this.progressSpinnerValue <= 100) {
+      this.progressSpinnerValue = this.getSpinnerValue(startTimeArr);
+      this.getRemainingTime();
     } else {
       clearInterval(this.timerId);
+      this.getRemainingTime();
+      this.progressSpinnerValue = 0;
       return;
     }
   }
 
-  private timeloop(startTimeArr: string[]) {
-    this.timerId = setInterval(this.setSpinnerValue.bind(this), 30000, startTimeArr);
+  private timeLoop(startTimeArr: string[]) {
+    this.timerId = setInterval(this.setSpinnerValue.bind(this), 60000, startTimeArr);
   }
 
   private getParseInt(startTime: string) {
     return Number.parseInt(startTime);
   }
 
-  private getFormattedTimestring(hour: number, minute: number) {
+  private getFormattedTimeString(hour: number, minute: number) {
     return formatDate(new Date().setHours(hour, minute), 'HH:mm', 'en');
   }
 
-  private getSpinnerMaxValue(work: string[], pause: string[]) {
-    let workduration = this.getParseInt(work[0]) * 60 + this.getParseInt(work[1])
-    let pauseduration = this.getParseInt(pause[0]) * 60 + this.getParseInt(pause[1])
+  private getSpinnerMaxValue() {
+    let workduration = this.getParseInt(this.workTimeArr[0]) * 60 + this.getParseInt(this.workTimeArr[1])
+    let pauseduration = this.getParseInt(this.pauseArr[0]) * 60 + this.getParseInt(this.pauseArr[1])
     this.maxSpinnerValue = workduration + pauseduration;
+  }
+
+  private getRemainingTime() {
+    let minutes = this.maxSpinnerValue - this.vergangen;
+    this.remaining = this.getFormattedTimeString(0, minutes);
   }
 
 }
