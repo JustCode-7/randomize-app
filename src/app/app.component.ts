3a0fa5389d273bb00c849f32bac5dd205e70ac86
JustCode-7 <72110343+JustCode-7@users.noreply.github.com>
2023-05-19 00:20:04 +0200

    feat: increase version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'randomize-app';
   appVersion: string = environment.appVersion;
 
   constructor() {
+  }
+
+  ngOnInit(): void {
+
+    //TODO: get  version from github-repsoitory
+    // let req: HttpRequest = new HttpRequest();
+    // req.set("Access-Control-Allow-Origin", "https://github.com/JustCode-7/randomize-app/blob/master/package.json");
+    // req.setHeaders()
+    // window
+    //   .fetch((input: { headers: { set: (arg0: string, arg1: string) => Promise<any>; }; })
+    //     => input.headers.set())
+    //   .then(r => {
+    //     console.log(r.json())
+    //   })
   }
 }
